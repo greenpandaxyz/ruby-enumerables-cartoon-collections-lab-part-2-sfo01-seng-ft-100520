8ede0e608e455789c839023fdc356badd56b469d
Amanda D <amanda@depaula.xyz>
2020-09-24 01:57:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 planeteer_calls.detect do |element|
-element == element
+element == planeteer_calls
   end
 end
 
