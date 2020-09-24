dcff395c93bd42ccfb176e0fbb253a3697bdf61c
Amanda D <amanda@depaula.xyz>
2020-09-24 01:50:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,6 @@ def find_valid_calls(planeteer_calls)
 planeteer_calls.any? do |element|
 element == valid_calls
   end
-  find_valid_calls(valid_calls)
 end
 
 # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
