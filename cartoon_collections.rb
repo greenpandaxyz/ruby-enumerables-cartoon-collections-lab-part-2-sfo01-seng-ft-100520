6ed584ec8913d0a11a312f08ce86990c4bcbe7af
Amanda D <amanda@depaula.xyz>
2020-09-24 02:02:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,8 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-planeteer_calls.any? do |element|
- element == element
+planeteer_calls.find do |element|
+element == valid_calls
   end
 end
 
